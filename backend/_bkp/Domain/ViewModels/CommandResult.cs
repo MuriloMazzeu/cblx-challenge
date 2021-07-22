eb89d6b6105877b9a7bb93528eac9d9831bfcbb9
Murilo Mazzeu <mazzeu.murilo@gmail.com>
2021-07-22 15:59:52 -0300

    remove bkp folder

--- backend/_bkp/Domain/ViewModels/CommandResult.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace CblxChallenge.Domain.ViewModels
-{
-    public class CommandResult
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-    }
-}
--- backend/_bkp/Domain/ViewModels/QueryResult.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace CblxChallenge.Domain.ViewModels
-{
-    public class QueryResult<T>
-    {
-        public bool Success { get; set; }
-        public T Data { get; set; }
-    }
-}
